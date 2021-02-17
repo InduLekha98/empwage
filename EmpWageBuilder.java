604fe07777b4a07bb527dfb1291c6964e38971ec
InduLekha98 <rindulekha09@gmail.com>
2021-02-18 01:47:55 +0530

    Added:Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,11 @@
 public class EmpWageBuilder{
 	public static void main(String args[]) {
 	  int isPresent=1;
-	  double empCheck= Math.floor(Math.random()*10)%2;
+          double empCheck= Math.floor(Math.random()*10)%2;
+	  int rateperhr=20;
+	  int Hr=8;
+	  int Salary=rateperhr*Hr;
+	  System.out.println("Salary="+Salary);
 	  if(empCheck == isPresent)
 		System.out.println("employee is present");
 	  else
